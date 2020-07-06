a1ea68ac465490327014398440d6142be778b9dc
kimelish <yoshuakim@gmail.com>
2020-07-06 10:38:40 -0700

    Add project files.

---
+++ ConferenceDTO/Speaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConferenceDTO
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [StringLength(4000)]
+        public string Bio { get; set; }
+
+        [StringLength(1000)]
+        public virtual string WebSite { get; set; }
+    }
+}
